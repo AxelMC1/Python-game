bc442e8547a8a8e8691e6bf02d6b4539a837afb4
AxelMC1 <190731227+AxelMC1@users.noreply.github.com>
2025-06-29 18:02:34 +0200

    Refactored by adding two widgets

--- test.py
+++ test.py
@@ -2,47 +2,91 @@ import sys
 import random
 from PySide6 import QtCore, QtWidgets, QtGui
 
+counter = 0
+
+class GeneratorWidget(QtWidgets.QWidget):
+    def __init__(self, game):
+        super().__init__()
+
+        self.inc = 1
+        self.game = game
+
+        self.button = QtWidgets.QPushButton(f"Get $${self.inc})")
+        self.button.clicked.connect(self.click)
+        
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.button)
+
+    @QtCore.Slot()
+    def click(self):
+        self.game.counter += self.inc
+        self.game.updateCounter()
+
+    def increment(self, amount):
+        self.inc += amount
+
+    
+    def update(self):
+        self.button.setText(f"Get $${self.inc})")
+
+class UpgradeWidget(QtWidgets.QWidget):
+    def __init__(self, game, cost, widget):
+        super().__init__()
+
+        self.cost = cost
+        self.inc = 1
+        self.incWidget = widget
+        self.game = game
+
+        self.button = QtWidgets.QPushButton(f"Upgrade (cost ${self.cost})")
+        self.button.clicked.connect(self.click)
+        
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.button)
+
+    @QtCore.Slot()
+    def click(self):
+        if self.game.counter < self.cost:
+            return
+        self.game.counter -= self.cost
+        self.incWidget.increment(self.inc)
+        self.game.updateCounter()
+
+    def increment(self, amount):
+        self.inc += amount
+
+    
+    def update(self):
+        pass
+
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
         self.counter = 0
-        self.plusnumb = 1
 
-        self.upgrade = QtWidgets.QPushButton("Upgrade (cost = $10)")
+        self.generator = GeneratorWidget(self)
+        self.upgrade = UpgradeWidget(self, 10, self.generator)
+        self.upgrade2 = UpgradeWidget(self, 500, self.upgrade)
         self.gamble = QtWidgets.QPushButton("Gamble (cost = $100)")
-        self.button = QtWidgets.QPushButton("press to get MONEY")
         self.text = QtWidgets.QLabel("$0",
                                      alignment=QtCore.Qt.AlignCenter)
 
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.generator)
         self.layout.addWidget(self.upgrade)
         self.layout.addWidget(self.gamble)
+        self.layout.addWidget(self.upgrade2)
 
-        self.button.clicked.connect(self.magic)
-        self.upgrade.clicked.connect(self.upgradefnc)
         self.gamble.clicked.connect(self.loser)
     
-    @QtCore.Slot()
-    def upgradefnc(self):
-        if self.counter >=10:
-            self.counter -= 10
-            self.plusnumb += 1
-            self.updateCounter()
-
     @QtCore.Slot()
     def loser(self):
         if self.counter >=100:
             self.counter -= 100
             self.counter += random.randrange(0, 12)**2
             self.updateCounter()
-
-    @QtCore.Slot()
-    def magic(self):
-        self.counter += self.plusnumb
-        self.updateCounter()
     
     def updateCounter(self):
         self.text.setText("$"+str(self.counter))
